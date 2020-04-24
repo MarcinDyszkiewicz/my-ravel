5efcfccff3288f1482364f5fa310f87f7c6b3319
MarcinDyszkiewicz <daveheseling@gmail.com>
2020-04-24 11:24:05 +0200

    added docker

---
+++ docker/mysql/init-scripts/000_init_database.sql
@@ -0,0 +1,4 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'klego';
+FlUSH PRIVILEGES;
+
+USE postclick_roles;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'my_travel',
+      password: 'secret',
+      database: 'my_travel_mysql',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
